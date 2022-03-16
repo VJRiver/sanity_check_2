725f7f214cfc3d3a4f4646636f13ef6dd888fc34
VJRiver <victorjavier0716@gmail.com>
2022-03-16 04:10:16 -0600

    "Controlada excepción lanzada cuando el usuario presionaba "Cancel" "

--- mensaje.java
+++ mensaje.java
@@ -1,9 +1,16 @@
 import javax.swing.*;
 public class mensaje {
 	public void saludar() {
+		
 		String nombre = JOptionPane.showInputDialog("Por favor introduce tu nombre");
-		if(nombre != null) {
-			JOptionPane.showMessageDialog(null,"Hola " + nombre);
+		try {
+			if(!nombre.isEmpty()) {
+				JOptionPane.showMessageDialog(null,"Hola " + nombre);
+			}else {
+				JOptionPane.showMessageDialog(null, "Hola extrañ@");
+			}
+		}catch(Exception e) {
+			JOptionPane.showMessageDialog(null, "Saludo cancelado");
 		}
-	}
+	}	// SALUDAR()
 }
