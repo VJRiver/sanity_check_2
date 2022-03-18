482c73843ab3878c26449283ca17b80699deae51
VJRiver <38737685+VJRiver@users.noreply.github.com>
2022-03-18 03:53:51 -0600

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,11 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		mensaje saludo = new mensaje();
+		saludo.saludar();
+		System.out.println("Buenas noches");
+	}
+
+}
---
+++ mensaje.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+public class mensaje {
+	public void saludar() {
+		
+		String nombre = JOptionPane.showInputDialog("Por favor introduce tu nombre");
+		try {
+			if(!nombre.isEmpty()) {
+				JOptionPane.showMessageDialog(null,"Hola " + nombre);
+			}else {
+				JOptionPane.showMessageDialog(null, "Hola extrañ@");
+			}
+		}catch(Exception e) {
+			JOptionPane.showMessageDialog(null, "Saludo cancelado");
+		}
+		
+	}	// SALUDAR()
+}
